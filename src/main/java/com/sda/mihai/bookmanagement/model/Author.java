ab843f95199558c78fc520976ba462205d02f667
Mihai Florian <mflorian90@yahoo.com>
2022-11-16 18:54:56 +0200

    Book and Author classes

---
+++ src/main/java/com/sda/mihai/bookmanagement/model/Author.java
@@ -0,0 +1,66 @@
+package com.sda.mihai.bookmanagement.model;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany(mappedBy = "author")
+    private ArrayList<Book> books;
+
+    public Author() {
+
+    }
+
+    public Author(String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/sda/mihai/bookmanagement/model/Book.java
@@ -0,0 +1,69 @@
+package com.sda.mihai.bookmanagement.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "author_id")
+    private Integer author_id;
+
+    @OneToMany
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book(){
+
+    }
+
+    public Book(String title, String description, Integer author_id) {
+        this.title = title;
+        this.description = description;
+        this.author_id = author_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", author_id=" + author_id +
+                '}';
+    }
+}
